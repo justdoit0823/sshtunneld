34ddb743b26eb5c9faeddf552c9ec9758e342aed
justdoit <justdoit@justdoitdeMacBook-Pro.local>
2015-05-18 12:00:07 +0800

    complete sysv init script.

--- sshtunnel.sh
+++ sshtunnel.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
+
+DAEMON="/usr/local/bin/sshtunnel"
+PIDFILE="/var/run/sshtunnel.pid"
+DAEMON_NAME="sshtunneld"
+CONFIGFILE="/etc/sshtunnel.conf"
+
+
 start()
 {
-	start-stop-daemon --start --nodook --name sshtunneld --pidfile /var/run/sshtunnel.pid --startas /usr/local/bin/sshtunnel --daemon
+	start-stop-daemon --start --oknodo --name $DAEMON_NAME --pidfile $PIDFILE --startas $DAEMON --daemon
 	echo "start sshtunneld server"
 }
 
 stop()
 {
-	start-stop-daemon --stop --nodook --name sshtunneld --pidfile /var/run/sshtunnel.pid --retry 5
+	start-stop-daemon --stop --oknodo --name $DAEMON_NAME --pidfile $PIDFILE --retry 5
 	echo "stop sshtunneld server"
 }
 
 status()
 {
-	start-stop-daemon --status --nodook --name sshtunneld --pidfile
-
+	start-stop-daemon --status --oknodo --name $DAEMON_NAME --pidfile $PIDFILE
+	if [ $? == 0 ]; then
+	    echo "sshtunnel daemon is running"
+	fi
 }
 
 
 check()
 {
-	if [ ! -x /usr/local/bin/sshtunnel ]; then
+	if [ ! -x $DAEMON ]; then
 
 		echo "can not execute sshtunnel"
 		exit 1
